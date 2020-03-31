555f89f3054545b0e920da96fe556013bef174a2
tosiran <f2.ree_eat_10470@icloud.com>
2020-03-31 14:46:44 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Mybookshelf
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+
+  end
+end
